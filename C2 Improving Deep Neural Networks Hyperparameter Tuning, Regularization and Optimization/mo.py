3ef6d91265e10df3811392f1d2e6ed83ea08ca06
MortezaKamrani <smorteza.kamrani@gmail.com>
2021-11-03 14:57:41 +0330

    C2W3DeleteVenvFromGit

--- C2 Improving Deep Neural Networks Hyperparameter Tuning, Regularization and Optimization/mo.py
+++ C2 Improving Deep Neural Networks Hyperparameter Tuning, Regularization and Optimization/mo.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-a = np.array([[1,2,3], [4,5,6]])
-print(a)
-print(a.shape)
-print(np.square(a))
+# a = np.array([[1,2,3], [4,5,6]])
+# print(a)
+# print(a.shape)
+# print(np.square(a))
